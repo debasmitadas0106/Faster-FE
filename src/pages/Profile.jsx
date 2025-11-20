2df8350d7acbf53afd04d21e38e3e2c0a11e31b6
Debasmita Das <debasmita@vodex.ai>
2025-11-21 03:54:23 +0530

    profile page a bit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,11 @@
+import "./Profile.css";
 function Profile() {
-  return <div>Hi this is my profile page</div>;
+  return (
+    <div className="profile-page">
+      <div className="profile-dp-container"></div>
+      <div className="profile-dp"></div>
+      <div className="profile-name">Debasmita Das</div>
+    </div>
+  );
 }
 export default Profile;
